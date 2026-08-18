06fc533db077c8372b4fde6547cb064991fb4ad8
GurusricharanVadaparthi <vgurusricharan1129@gmail.com>
2026-08-18 20:56:34 +0530

    Update settings.py

--- todo/settings.py
+++ todo/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-kpje9966h3o@9p&c%4@nwylo+wu9!nm@&e1y(yc99g&*y5j5xg
 
 DEBUG = True
 
-ALLOWED_HOSTS = ["task-flow-1-0wwe.onrender.com"]
+ALLOWED_HOSTS = ["task-flow-3-0wwe.onrender.com"]
 
 # --------------------------------------------------
 # Installed Applications
